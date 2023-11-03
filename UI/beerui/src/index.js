2c6b7e9af8233ff5153f0a13bdcc2e72dfb8ad7c
Hugh Mungus <mtlnielsen@gmail.com>
2023-11-03 12:00:32 +0100

    feat: InitialsBorder

--- UI/beerui/src/index.js
+++ UI/beerui/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Text from './Text';
 import Button from './Button';
+import InitialsBorder from './InitialsBorder'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Button label="Prepare Game" />);
+root.render(<InitialsBorder/>);
