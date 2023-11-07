7d259adf50fe20fe0b7ef99b7457c6911949eef8
Hugh Mungus <mtlnielsen@gmail.com>
2023-11-07 10:30:11 +0100

    fix: initialsBorder

--- UI/beerui/src/InitialsBorder.jsx
+++ UI/beerui/src/InitialsBorder.jsx
@@ -7,16 +7,19 @@ function InitialsBorder(props) {
     const borderStyle = {
         width: width || '578px',
         height: height || '122px',
-        //flex-shrink: '0px',
         borderRadius: borderRadius || '23px',
         border: border || '1px solid #000',
         background: background || '#000',
         boxShadow: boxShadow || '0px 4px 4px 0px rgba(0, 0, 0, 0.25)',
         display: 'flex',
         justifyContent: 'center',
-        alignItems: 'center',     
+        alignItems: 'center',
     };
-    return (<border className="border" style={borderStyle}/>);
+    
+    
+    return (
+        <div className="border" style={borderStyle} />
+    );
 }
 
 export default InitialsBorder;
